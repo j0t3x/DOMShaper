144160dbf0165b96cafa3f93b5be18dd7b1dd2b8
Jose palacios <jmpd1988@gmail.com>
2017-02-13 20:58:37 -0500

    avanzando formshape, ahora ya tiene basic building of form sections y serializacion recursiva de data basada en shapes

--- src/shapes/FormShape.js
+++ src/shapes/FormShape.js
@@ -1,4 +1,7 @@
 var Shape = require('./Shape.js');
+var TextInputShape = require('./TextInputShape.js');
+var ButtonShape = require('./ButtonShape.js');
+var OptionShape = require('./OptionShape.js');
 
 var FormShape = function( action, id ){
   Shape.call( this, 'form', id );
@@ -6,7 +9,8 @@ var FormShape = function( action, id ){
   this.action = action;
   this.method = '';
   this.encoding = '';
-  this.elements = [];
+  this.submitTrigger;
+  this.data; //remember this could be a file
 
 };
 /*OOP herency*/
@@ -34,6 +38,16 @@ FormShape.prototype.buildDom = function(){
   if( this.encoding )
     this.domElement.enctype = this.encoding;
 
+  if( !this.submitTrigger ){
+    throw 'You need a submit trigger for a form';
+  }
+
+  //make submitTrigger button visible or invisible according to the flag setted at creation
+  if( !this.submitTrigger.visible )
+    this.submitTrigger.domElement.style.visibility = "hidden";
+  //inject our submit code
+  this.submitTrigger.on( 'click', this.sendData.bind(this) );
+
 };
 
 
@@ -55,6 +69,84 @@ FormShape.prototype.setEncoding = function(encoding){
 
   if( encoding )
     this.encoding = encoding;
+
+};
+
+FormShape.prototype.addSection = function( childSection ){
+
+  if( childSection instanceof Shape ){
+
+    this.appendShape( childSection );
+    console.log( this.childs );
+
+  }else{
+    throw 'The argument should be a Shape object';
+  }
+
+};
+
+FormShape.prototype.setSubmitTrigger = function( text ){
+  //un trigger puede ser un boton, como puede ser un TECLA
+  if( typeof text !== 'string' )
+    throw 'Argument should be a string containing text of submit button, if no text submit will be not visible';
+
+  var st;
+  if ( text !== '' ) {
+    st = new ButtonShape( text );
+    //lets set a flag in this object to account for ti later on buildDom
+    st.visible = true;
+  }else{
+    st = new ButtonShape('');
+  }
+
+  this.submitTrigger = st;
+  this.appendShape( this.submitTrigger );
+
+  return st;
+};
+
+FormShape.prototype.sendData = function( evt ){
+
+  evt.preventDefault();
+  //TODO: lib that structure requests for our framework :)
+
+};
+
+FormShape.prototype.serializeForm = function(){
+
+  this.serializeElement( this );
+  return this.data;
+
+};
+
+FormShape.prototype.serializeElement = function( theShape ){
+
+  for( var i = 0; i < theShape.length; ++i ) {
+
+    var name;
+    var value;
+
+    var element = theShape[i];
+
+    if( element instanceof TextInputShape ){
+      name = element.id;
+      value = element.getVal();
+    }else if ( element instanceof OptionShape ) {
+      name = element.id;
+      value = element.getSelectedOptions();
+    }else{
+      continue;
+    }
+
+    if( name ) {
+       this.data[ name ] = value;
+    }
+
+    if( theShape.childs.length > 0 ){
+      this.serializeElement( theShape[i] );
+    }
+
+  }
 
 };
 
