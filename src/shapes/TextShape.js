07e08c145e51a3acfd1a6d113c8a63b45e71e0bb
renziito <renzo.aliaga@easytaxi.com.pe>
2017-02-24 13:07:58 -0500

    adding param method on updateText, this affect the way the text is implemented (append, prepend, replace [default])

--- src/shapes/TextShape.js
+++ src/shapes/TextShape.js
@@ -1,59 +1,45 @@
 var Shape = require('./Shape.js');
-
-var TextShape = function( elem, id ){
-  Shape.call( this, elem, id );
-  this.textContent = '';
+var TextShape = function(elem, id) {
+    Shape.call(this, elem, id);
+    this.textContent = '';
 };
 /*OOP herency*/
-TextShape.prototype = Object.create( Shape.prototype );
+TextShape.prototype = Object.create(Shape.prototype);
 TextShape.prototype.contructor = TextShape;
 /*OOP herency*/
-
-TextShape.prototype.buildDom = function(){
-
-  if( this.elementName && !this.domElement )
-    this.domElement = document.createElement( this.elementName );
-
-  this.domElement.innerHTML = this.textContent;
-
-  if( this.id )
-    this.domElement.id = this.id;
-
-  if( this.classes )
-    this.domElement.className += ' ' + this.classes;
-
-  for (var i = 0; i < this.childs.length; i++) {
-
-    if( !this.childs[i].domElement ){
-      this.childs[i].buildDom();
-    }
-
-
-    this.domElement.appendChild( this.childs[i].domElement );
-
-  }
-
-
-};
-
-//t is a string with the text for the textShape
-TextShape.prototype.updateText = function( textContent ){
-
-    this.needsReRender = true;
-
-    if( typeof textContent !== 'string' )
-      console.error(': argument of updateText should be a string literal');
-
-    this.textContent = textContent;
-
-};
-
-TextShape.prototype.render_ = function(){
-
-  if( this.textContent )
+TextShape.prototype.buildDom = function() {
+    if (this.elementName && !this.domElement) this.domElement = document.createElement(this.elementName);
     this.domElement.innerHTML = this.textContent;
-
+    if (this.id) this.domElement.id = this.id;
+    if (this.classes) this.domElement.className += ' ' + this.classes;
+    for (var i = 0; i < this.childs.length; i++) {
+        if (!this.childs[i].domElement) {
+            this.childs[i].buildDom();
+        }
+        this.domElement.appendChild(this.childs[i].domElement);
+    }
 };
-
-
+//t is a string with the text for the textShape
+TextShape.prototype.updateText = function(textContent, method = "replace") {
+    this.needsReRender = true;
+    if (typeof textContent !== 'string') {
+        console.error(': first argument of updateText should be a string literal');
+    }
+    switch (method) {
+        case "replace":
+            this.textContent = textContent;
+            break;
+        case "append":
+            this.textContent = this.textContent + textContent;
+            break;
+        case "prepend":
+            this.textContent = textContent + this.textContent;
+            break;
+        default:
+            console.error(': method of updateText invalid, valid methods : [default] "replace","append","prepend"');
+    }
+};
+TextShape.prototype.render_ = function() {
+    if (this.textContent) this.domElement.innerHTML = this.textContent;
+};
 module.exports = TextShape;
